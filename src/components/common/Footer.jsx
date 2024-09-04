14884ece1e9b03efc92570431df890e7daa77983
ryanny <ryannymromero@gmail.com>
2024-09-04 11:17:42 +0400

    Change the footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="bg-dark text-white">
       <div className="container flex flex-col text-center sm:text-start sm:flex-row justify-between gap-1 text-xs py-4 font-medium">
         <p>Copyright &copy; 2023 Villa Crisanta Spring Resort - All Rights Reserved.</p>
-        <p>Powered by GABO</p>
+        <p>Powered by GABO v1.2</p>
       </div>
     </footer>
   )
